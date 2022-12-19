b111940576dc21996736204918ce7b9a664a9b30
HieuNguyen305 <ngmminhhieu@gmail.com>
2022-12-19 12:04:06 +0700

    upload sc bai tap layout

---
+++ src/App.js
@@ -0,0 +1,14 @@
+// import logo from './logo.svg';
+import "./App.css";
+import BaiTapThucHanhLayOut from "./BaitapThucHanhLayOut_Component";
+
+function App() {
+  return (
+    <div>
+      {/* Gọi lại component */}
+      <BaiTapThucHanhLayOut />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/BaitapThucHanhLayOut_Component/index.js
@@ -0,0 +1,8 @@
+import React, { Component } from "react";
+
+class BaiTapThucHanhLayOut extends Component {
+  render() {
+    return <div>BaiTapThucHanhLayOut</div>;
+  }
+}
+export default BaiTapThucHanhLayOut;
